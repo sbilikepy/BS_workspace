3db7564b237928d3a1a150029ab7b4382bb3ef59
Serhiienko Bohdan <serhiienko.b.i@gmail.com>
2023-09-21 23:31:40 +0300

    daily

--- Lessons/Django_ORM/daily.py
+++ Lessons/Django_ORM/daily.py
@@ -491,4 +491,15 @@ def you_are_a_square(number: int) -> bool:
     return (int(math.sqrt(number))) == math.sqrt(number)
 
 
-print(you_are_a_square(40))
+def who_likes_it(names: list) -> str:
+    if len(names) <= 0:
+        return "no one likes this"
+    if len(names) == 1:
+        return f"{names[0]} likes this"
+    if len(names) == 2:
+        return (f"{names[0]} and {names[1]}"
+                " like this")
+    if len(names) == 3:
+        return (f"{names[0]}, {names[1]} and {names[2]} like this")
+    if len(names) > 3:
+        return (f"{names[0]}, {names[1]} and {len(names) - 2} others like this")
