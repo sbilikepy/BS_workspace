aa3d0bc5f5d961814bbcf61ffe0f61ab1cc1a7f5
Serhiienko Bohdan <serhiienko.b.i@gmail.com>
2023-10-30 23:59:46 +0200

    daily

--- Lessons/HTML + CSS basics/daily_html_css.py
+++ Lessons/HTML + CSS basics/daily_html_css.py
@@ -30,3 +30,15 @@ def calculate(string: str) -> str:
 
 if __name__ == "__main__":
     calculate("123plus2plus3plus4")  # 10
+
+
+def histogram(results: list) -> str:
+    return (f" \"\"\"6|{' ' if results[-1] == 0 else '#' * results[-1] + ' ' + str(results[-1])}\n"
+            f"5|{' ' if results[-2] == 0 else '#' * results[-2] + ' ' + str(results[-2])}\n"
+            f"4|{' ' if results[-3] == 0 else '#' * results[-3] + ' ' + str(results[-3])}\n"
+            f"4|{' ' if results[-4] == 0 else '#' * results[-4] + ' ' + str(results[-4])}\n"
+            f"4|{' ' if results[-5] == 0 else '#' * results[-5] + ' ' + str(results[-5])}\n"
+            f"4|{' ' if results[-6] == 0 else '#' * results[-6] + ' ' + str(results[-6])}\n\"\"\"\n")
+
+
+print(histogram([7, 3, 10, 1, 0, 5]))
