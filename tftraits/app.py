a2f83bd1d5fab482aa39cd2db3c06605252ea578
Serhiienko Bohdan <serhiienko.b.i@gmail.com>
2023-11-23 17:26:43 +0200

    test deploy

--- tftraits/app.py
+++ tftraits/app.py
@@ -1,6 +1,8 @@
+from flask import Flask, render_template, request, redirect
 from datetime import datetime
-
 from data import traits, champions, composition_iterator
+
+app = Flask(__name__)
 
 current_composition = []
 current_traits = {}
@@ -23,6 +25,20 @@ def timer(func):
     return wrapper
 
 
+@app.route('/reset', methods=['POST'])
+def reset():
+    current_composition.clear()
+    current_traits.clear()
+    capped_traits.clear()
+    breakpoint_traits.clear()
+    remaining_traits.clear()
+    first_prior.clear()
+    second_prior.clear()
+    third_prior.clear()
+    no_prior.clear()
+    return redirect('/')
+
+
 @timer
 def data_fill():
     for composition in composition_iterator:
@@ -32,47 +48,47 @@ def data_fill():
             champions[champion].append(trait_name)
 
 
-@timer
-def request():
-    print("Enter \"help\" to calculate")
-
-    flag = True
-    while flag:
-
-        character = input("Character name: ").lower()
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        character = request.form['character'].lower()
         if character.capitalize() in current_composition:
             print(f"{character.capitalize()} already here")
-        if character == "help":
-            return
-
-        if character.capitalize() == "Akali":
-            if "Akali" not in current_composition:
-                akali_spec = None
-                while akali_spec not in ("1", "2"):
-                    akali_spec = input("\nAkali spec: \n1. K/DA\n2. True Damage\n")
-
-                if akali_spec == "1":
-                    champions["Akali"].append("K/DA")
-                    print("K/DA Akali has been added\n")
-                    current_composition.append("Akali")
-                if akali_spec == "2":
-                    champions["Akali"].append("True Damage")
-                    print("True Damage Akali has been added\n")
-                    current_composition.append("Akali")
-
-
+        elif character.capitalize() == "help":
+            return render_template('index.html',
+                                   composition=current_composition,
+                                   capped_traits=capped_traits,
+                                   breakpoint_traits=breakpoint_traits,
+                                   remaining_traits=remaining_traits,
+                                   first_prior=first_prior,
+                                   second_prior=second_prior,
+                                   third_prior=third_prior,
+                                   no_prior=no_prior,
+                                   suggestions=None)
+        elif character.capitalize() == "akali":
+            akali()
+        elif any(character == champ.lower() for champ in champions.keys()):
+            if character.lower() not in [i.lower() for i in current_composition]:
+                current_composition.append(character.capitalize())
+                print(f"{character.capitalize()} has been added\n")
         else:
-            if any(character == champ.lower() for champ in champions.keys()):
-                if character.lower() not in [i.lower() for i in current_composition]:
-                    current_composition.append(character.capitalize())
-                    print(f"{character.capitalize()} has been added\n")
-            else:
-                print("Enter a valid character\n")
+            print("Enter a valid character\n")
+
+    group_data()
+    suggestions = tailor()
+    return render_template('index.html',
+                           composition=current_composition,
+                           capped_traits=capped_traits,
+                           breakpoint_traits=breakpoint_traits,
+                           remaining_traits=remaining_traits,
+                           first_prior=first_prior,
+                           second_prior=second_prior,
+                           third_prior=third_prior,
+                           no_prior=no_prior,
+                           suggestions=suggestions)
 
 
-@timer
-def group():
-
+def group_data():
     current_traits.clear()
     capped_traits.clear()
     breakpoint_traits.clear()
@@ -102,7 +118,6 @@ def group():
             breakpoint_traits.append(f"{trait}: {count} | BREAKPOINT [+{next_upgrade}]")
             if next_upgrade == 1:
                 first_prior.append(trait)
-
         else:
             remaining_traits.append(f"{trait}: {count} [+{traits[trait][0] - count}]")
             if (traits[trait][0] - count) == 1:
@@ -110,29 +125,15 @@ def group():
             if (traits[trait][0] - count) > 1:
                 third_prior.append(trait)
 
-    print("Your traits:")
 
-    for capped_trait in capped_traits:
-        print(capped_trait)
-
-    for breakpoint_trait in breakpoint_traits:
-        print(breakpoint_trait)
-
-    sorted_remaining_traits = sorted(remaining_traits, key=lambda x: traits[x.split(':')[0]][0] - int(x.split(':')[1].split()[0]))
-    for sorted_remaining_trait in sorted_remaining_traits:
-        print(sorted_remaining_trait)
-    tailor()
-
-@timer
 def tailor():
     print(f"\n\nFirst prio: {first_prior}")
     print(f"Second prio: {second_prior} ")
     print(f"Third prio: {third_prior}")
     print(f"No prio: {no_prior}\n\n")
     suggestions = {
-        name : 0 for name in champions.keys()
+        name: 0 for name in champions.keys()
     }
-
 
     for trait in first_prior + second_prior + third_prior + no_prior:
 
@@ -155,7 +156,6 @@ def tailor():
                     if trait in traits:
                         suggestions[name] += 0
 
-
     suggestions = dict(sorted(suggestions.items(), key=lambda item: item[1], reverse=True))
 
     scoreboard = {}
@@ -166,39 +166,29 @@ def tailor():
         else:
             scoreboard[score].append(character)
 
+    result = {}
     for score, characters in scoreboard.items():
-        print(f"{score} points: {[', '.join(characters)]}")
-    flag_next = False
-    while flag_next is not True:
-        choice = input("\n\nYour choice: ")
-        if choice.capitalize() == "Akali":
-            if "Akali" not in current_composition:
-                akali_spec = None
-                while akali_spec not in ("1", "2"):
-                    akali_spec = input("\nAkali spec: \n1. K/DA\n2. True Damage\n")
+        result[score] = characters
 
-                if akali_spec == "1":
-                    champions["Akali"].append("K/DA")
-                    print("K/DA Akali has been added\n")
-                    current_composition.append("Akali")
-                if akali_spec == "2":
-                    champions["Akali"].append("True Damage")
-                    print("True Damage Akali has been added\n")
-                    current_composition.append("Akali")
+    return result
 
-        if choice not in current_composition:
-            if choice.capitalize() in champions.keys():
-                current_composition.append(choice.capitalize())
-                flag_next = True
-                group()
 
+def akali():
+    if "Akali" not in current_composition:
+        akali_spec = None
+        while akali_spec not in ("1", "2"):
+            akali_spec = input("\nAkali spec: \n1. K/DA\n2. True Damage\n")
+
+        if akali_spec == "1":
+            champions["Akali"].append("K/DA")
+            print("K/DA Akali has been added\n")
+            current_composition.append("Akali")
+        if akali_spec == "2":
+            champions["Akali"].append("True Damage")
+            print("True Damage Akali has been added\n")
+            current_composition.append("Akali")
 
 
 if __name__ == "__main__":
     data_fill()
-
-    print("\n")
-    print("*" * 50, "\n")
-    request()
-    group()
-
+    app.run(debug=True)
