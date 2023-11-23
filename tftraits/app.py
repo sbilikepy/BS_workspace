fe3918053faf44527145e96ca87098cee62a5356
Serhiienko Bohdan <serhiienko.b.i@gmail.com>
2023-11-23 19:36:59 +0200

    sessions fix

--- tftraits/app.py
+++ tftraits/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, session
+from flask import Flask, render_template, request, redirect, session, jsonify
 from datetime import datetime
 from data import traits, champions, composition_iterator
 import os
