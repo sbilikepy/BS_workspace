51b6d7e66bf2a8b332eb386e0714514a7da6f062
Serhiienko Bohdan <serhiienko.b.i@gmail.com>
2023-12-19 04:06:42 +0200

    huge work :)

--- pw_recruit/pw_recruitment/settings.py
+++ pw_recruit/pw_recruitment/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "django_extensions",
+    "search_module"
 ]
 
 MIDDLEWARE = [
@@ -121,3 +123,4 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+AUTH_USER_MODEL = 'search_module.User'
--- pw_recruit/search_module/models.py
+++ pw_recruit/search_module/models.py
@@ -1,33 +1,78 @@
+from django.core.exceptions import ValidationError
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
+# https://pyrewood-village.eu/
+
+
 class User(AbstractUser):
     """Guid owner, who manage recruiting part"""
-    owner = models.BooleanField()
+
     guild = models.ForeignKey(
-        Guild, default=None, blank=True, on_delete=models.PROTECT, related_name="users_guild"
+        "Guild",
+        default=None,
+        blank=True,
+        null=True,
+        on_delete=models.CASCADE,
+        related_name="users_guild",
     )
+
+
+class InGameClass(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+
+class InGameSpec(models.Model):
+    name = models.CharField(max_length=255, unique= True) #TODO: check for Frost Holy  Protection
+
+
+class Character(models.Model):
+    class_name = models.ForeignKey(
+        InGameClass, on_delete=models.CASCADE, related_name="character_class"
+    )
+    spec_name = models.ForeignKey(
+        InGameSpec, on_delete=models.CASCADE, related_name="character_spec"
+    )
+
+
+class Recruit(models.Model):
+    nickname = models.CharField(max_length=255, blank=False, null=False)
+    character = models.ForeignKey(Character, on_delete=models.CASCADE)
+    note = models.CharField(max_length=255, blank=True, null=True)
+    wcl = models.IntegerField(
+        blank=True, null=True
+    )  # TODO: WCL API sync
+
+
+class Team(models.Model):
+    guild = models.ForeignKey("Guild", on_delete=models.CASCADE)
+    team_name = models.CharField(max_length=255)
+    team_size = models.IntegerField()
+    team_progress = models.IntegerField(default=0)
+    active_search = models.BooleanField(default=True)
+    looking_for = models.ManyToManyField(Character, related_name="looking_for")
 
 
 class Guild(models.Model):
     """Main list object"""
+
     name = models.CharField(max_length=255)
     owner = models.ForeignKey(
-        User, on_delete=models.PROTECT, related_name="guild_owner"
+        "User",
+        on_delete=models.CASCADE,
+        related_name="guild_owner",
+        blank=False,
+        null=False,
     )
-    highest_progress = models.CharField(max_length=5)  #TODO: set to max(Guild.team.team_progress)
-    teams = models.ForeignKey(Team, default=None, blank=True, on_delete=models.PROTECT, related_name="guid_teams")
-
-
-class Team(models.Model):
-    guild = models.ForeignKey(Guild, on_delete=models.PROTECT)
-    team_name = models.CharField(max_length=255)
-    team_size = models.IntegerField()
-    team_progress = models.IntegerField()
-    lf_players = models.BooleanField()
-
-
-class GameClass(models.Model):
-    class_name = models.CharField(max_length=255)
-    class_spec = models.CharField(max_length=255)
+    highest_progress = models.CharField(
+        max_length=5
+    )  # TODO: set to max(Guild.team.team_progress)
+    teams = models.ForeignKey(
+        Team,
+        default=None,
+        blank=True,
+        null=True,
+        on_delete=models.PROTECT,
+        related_name="guid_teams",
+    )
