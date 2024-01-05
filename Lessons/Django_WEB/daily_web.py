694482e13e8b634cecb479df9637052a2e7b3683
Serhiienko Bohdan <serhiienko.b.i@gmail.com>
2024-01-05 22:47:59 +0200

    daily web

--- Lessons/Django_WEB/daily_web.py
+++ Lessons/Django_WEB/daily_web.py
@@ -43,8 +43,7 @@ def pluck(dicts: list, name: str) -> list:
 
 
 def xo(string: str) -> bool:
-    return True if string.lower().count("x") == string.lower().count("o") \
-        else False
+    return True if string.lower().count("x") == string.lower().count("o") else False
 
 
 class PaginationHelper:
@@ -57,8 +56,9 @@ class PaginationHelper:
 
         for i in range(0, (len(self.collection) // items_per_page) + 1):
             self.content.append(
-                self.collection[self.iter_index:
-                                self.iter_index + self.items_per_page:]
+                self.collection[
+                self.iter_index: self.iter_index + self.items_per_page:
+                ]
             )
             self.iter_index += items_per_page
 
@@ -95,7 +95,7 @@ class PaginationHelper:
             return -1
 
 
-helper = PaginationHelper(['a', 'b', 'c', 'd', 'e', 'f'], 4)
+helper = PaginationHelper(["a", "b", "c", "d", "e", "f"], 4)
 helper.page_index(5)
 helper.page_index(2)
 helper.page_index(20)
@@ -161,7 +161,8 @@ def valpa():
                 str(random.randint(1, 2)),
                 str(random.randint(1, 2)),
                 chr(random.randint(97, 122)).upper(),
-                chr(random.randint(97, 122)).lower()]
+                chr(random.randint(97, 122)).lower(),
+            ]
         )
     return result
 
@@ -225,9 +226,7 @@ def find_unknown_number(first: int, second: int, third: int) -> int:
                     return i
 
 
-def discover_original_price(
-        discounted_price: float, sale_percentage: float
-) -> float:
+def discover_original_price(discounted_price: float, sale_percentage: float) -> float:
     return round((discounted_price / (100 - sale_percentage)) * 100, 2)
 
 
@@ -235,10 +234,11 @@ def buy_tofu(cost: int, box: str) -> list or str:
     print(cost)
     print(box)
     box = box.split()
-    result = [box.count("mon"),
-              box.count("monme"),
-              box.count("mon") + box.count("monme") * 60,
-              ]
+    result = [
+        box.count("mon"),
+        box.count("monme"),
+        box.count("mon") + box.count("monme") * 60,
+    ]
     min_amount = 0
     if result[1] > 0:
         for i in range(result[1]):
@@ -363,88 +363,10 @@ def is_happy(number):
 
 
 # flake8: noqa E501
-WORDS = ["ACT", "ADD", "ALL", "APE", "AND", "ANN", "ANY", "ANT", "ARE", "ART", "ASS", "BAD", "BAR", "BAT", "BAY", "BEE", "BIG", "BIT", "BOB", "BOY", "BUN",
-         "BUT", "CAN", "CAR", "CAT", "COT", "COW", "CUT", "DAD", "DAY", "DEW", "DID", "DIN", "DOG", "DON", "DOT", "DUD", "EAR", "EAT", "EEL", "EGG", "ERR",
-         "EYE", "FAG", "FAR", "FLY", "FOR", "FUN", "FUR", "GAY", "GET", "GOT", "GUM", "GUN", "GUY", "GUT", "GYM", "HAS", "HAT", "HER", "HEY", "HIM", "HIS",
-         "HIT", "HOW", "HUG", "HUN", "ICE", "INK", "ITS", "IVE", "JAN", "JET", "JOB", "JOT", "JOY", "KEY", "LAP", "LAY", "LIE", "LET", "LOG", "MAN", "MAP",
-         "MAY", "MEN", "MOM", "MUD", "MUM", "NAP", "NEW", "NOD", "NOT", "NOW", "OAR", "ODD", "OFF", "OLD", "ONE", "OUR", "OUT", "PAN", "PAL", "PAT", "PAW",
-         "PEN", "PET", "PIG", "PIT", "POT", "PRO", "PUT", "QUO", "RAG", "RAM", "RAN", "RAP", "RAT", "RED", "RIP", "ROD", "ROT", "RUN", "RUT", "SAT", "SAW",
-         "SAY", "SEA", "SEE", "SEX", "SHE", "SOY", "SUN", "SUX", "TAN", "TAT", "TEA", "THE", "TIN", "TIP", "TIT", "TON", "TOP", "TOO", "TWO", "URN", "USE",
-         "VAN", "VET", "VIP", "WAR", "WAS", "WAY", "WED", "WHO", "WHY", "WIN", "WON", "XXX", "YAK", "YAM", "YAP", "YOU", "YUM", "ZAP", "ZIP", "ZIT", "ZOO",
-         "ABLE", "ACED", "AGOG", "AHEM", "AHOY", "ALLY", "AMEN", "ANTI", "ANTS", "ANUS", "APES", "ARMY", "ARSE", "ARTY", "AVID", "AWED", "BABY", "BARS",
-         "BATS", "BAYS", "BEAR", "BEES", "BILL", "BITE", "BITS", "BLOW", "BLUE", "BOLD", "BONE", "BOOB", "BOOM", "BOSS", "BOYS", "BUFF", "BUNG", "BUNS",
-         "BUMS", "BURP", "BUST", "BUSY", "BUZZ", "CANS", "CANT", "CARS", "CART", "CATS", "CHAP", "CHIC", "CHUM", "CIAO", "CLAP", "COCK", "CODE", "COOL",
-         "COWS", "COZY", "CRAB", "CREW", "CURE", "CULT", "DADS", "DAFT", "DAWN", "DAYS", "DECK", "DEED", "DICK", "DING", "DOGS", "DOTS", "DOLL", "DOLT",
-         "DONG", "DOPE", "DOWN", "DRAW", "DUCK", "DUDE", "DUMB", "DUTY", "EARL", "EARN", "EARS", "EASY", "EATS", "EDGE", "EELS", "EGGS", "ENVY", "EPIC",
-         "EYES", "FACE", "FAGS", "FANG", "FARM", "FART", "FANS", "FAST", "FEAT", "FEET", "FISH", "FIVE", "FIZZ", "FLAG", "FLEW", "FLIP", "FLOW", "FOOD",
-         "FORT", "FUCK", "FUND", "GAIN", "GEEK", "GEMS", "GIFT", "GIRL", "GIST", "GIVE", "GLEE", "GLOW", "GOLD", "GOOD", "GOSH", "GRAB", "GRIN", "GRIT",
-         "GROT", "GROW", "GRUB", "GUNS", "GUSH", "GYMS", "HAIL", "HAIR", "HALO", "HANG", "HATS", "HEAD", "HEAL", "HEIR", "HELL", "HELP", "HERE", "HERO",
-         "HERS", "HIGH", "HIRE", "HITS", "HOLY", "HOPE", "HOST", "HUNK", "HUGE", "HUNG", "HUNS", "HURT", "ICON", "IDEA", "IDLE", "IDOL", "IOTA", "JAZZ",
-         "JERK", "JESS", "JETS", "JINX", "JOBS", "JOHN", "JOKE", "JUMP", "JUNE", "JULY", "JUNK", "JUST", "KATA", "KEYS", "KICK", "KIND", "KING", "KISS",
-         "KONG", "KNOB", "KNOW", "LARK", "LATE", "LEAN", "LICE", "LICK", "LIKE", "LION", "LIVE", "LOGS", "LOCK", "LONG", "LOOK", "LORD", "LOVE", "LUCK",
-         "LUSH", "MAKE", "MANY", "MART", "MATE", "MAXI", "MEEK", "MIKE", "MILD", "MINT", "MMMM", "MOMS", "MOOD", "MOON", "MOOT", "MUCH", "MUFF", "MUMS",
-         "MUTT", "NAPS", "NAZI", "NEAT", "NECK", "NEED", "NEWS", "NEXT", "NICE", "NICK", "NOON", "NOSE", "NOTE", "OARS", "OATS", "ONCE", "ONLY", "OPEN",
-         "ORGY", "OVAL", "OVER", "PANS", "PALS", "PART", "PAST", "PATS", "PAWS", "PEAR", "PERT", "PENS", "PETS", "PHEW", "PIPE", "PIPS", "PLAN", "PLUM",
-         "PLUS", "POET", "POOF", "POOP", "POSH", "POTS", "PROS", "PSST", "PUKE", "PUNK", "PURE", "PUSH", "PUSS", "QUAD", "QUAK", "QUID", "QUIT", "RANT",
-         "RAPE", "RAPS", "RAPT", "RATE", "RAMS", "RATS", "REAP", "RICK", "RING", "RIPE", "ROOT", "ROSE", "ROSY", "ROTS", "RUNT", "RUTS", "SAFE", "SAGE",
-         "SANE", "SAVE", "SAWS", "SEEK", "SEXY", "SHAG", "SHIT", "SICK", "SIGH", "SIRE", "SLAG", "SLIT", "SLUT", "SNAP", "SNOG", "SNUG", "SOFT", "SOON",
-         "SOUL", "SOUP", "SPRY", "STIR", "STUN", "SUCK", "SWAG", "SWAY", "TACT", "TANK", "TANS", "THAT", "THIS", "TIME", "TINS", "TINY", "TITS", "TOES",
-         "TONS", "TONY", "TOPS", "TOYS", "UBER", "URNS", "USED", "USER", "USES", "VAIN", "VAMP", "VARY", "VEIN", "VENT", "VERY", "VEST", "VIEW", "VIVA",
-         "VOLT", "VOTE", "WAFT", "WAGE", "WAKE", "WALK", "WALL", "WANG", "WANK", "WANT", "WARD", "WARM", "WARP", "WARS", "WART", "WASH", "WAVE", "WEAR",
-         "WEDS", "WEED", "WEEN", "WELD", "WHAT", "WHEE", "WHEW", "WHIP", "WHIZ", "WHOA", "WIFE", "WILL", "WIND", "WING", "WINK", "WINS", "WIRE", "WISH",
-         "WITH", "WORD", "WORK", "WRAP", "XMAN", "XMEN", "XRAY", "XTRA", "XXXX", "YANK", "YAKS", "YAMS", "YAPS", "YARD", "YARN", "YELP", "YERN", "YOKE",
-         "YOLK", "YULE", "ZANY", "ZAPS", "ZIPS", "ZITS", "ZERO", "ZOOM", "ZOOS"]
-
-
-def check_1800(string: str) -> set:
-    panel = {
-        "2": "ABC",
-        "3": "DEF",
-        "4": "GHI",
-        "5": "JKL",
-        "6": "MNO",
-        "7": "PQRS",
-        "8": "TUV",
-        "9": "WXYZ",
-    }
-
-    codes = {str(i): [] for i in range(10_000)}
-    result = []
-
-    for word in WORDS:
-        code = ""
-        for letter in word:
-            for number in panel:
-                if letter in panel[number]:
-                    code += number
-        codes[code].append(word)
-
-    cleared = {
-        k: codes[k] for k in codes.keys() if len(codes[k])
-    }
-    del codes
-
-    code_from_string_1 = ""
-    code_from_string_2 = ""
-    for word in string.split("-")[2::]:
-
-        for letter in word:
-            for button in panel:
-                if letter in panel[button]:
-                    code_from_string_1 += button
-    for word in string.split("-")[2::][::-1]:
-
-        for letter in word:
-            for button in panel:
-                if letter in panel[button]:
-                    code_from_string_2 += button
-    return None
-
-
-check_1800("1-800-INK-WANT")
 
 
 # "{'1-800-HOLY-COT', '1-800-INK-WANT', '1-800-HOLY-ANT'}"
+
 
 def make_readable(seconds: int) -> str:
     if seconds > 359999:
@@ -496,7 +418,7 @@ def tv_remote(word: str) -> int:
         ["f", "g", "h", "i", "j", "4", "5", "6"],
         ["k", "l", "m", "n", "o", "7", "8", "9"],
         ["p", "q", "r", "s", "t", ".", "@", "0"],
-        ["u", "v", "w", "x", "y", "z", "_", "/"]
+        ["u", "v", "w", "x", "y", "z", "_", "/"],
     ]
     steps = 0
     current_position = [0, 0]  # Починаємо з літери "a"
@@ -505,11 +427,95 @@ def tv_remote(word: str) -> int:
         for i, row in enumerate(all_rows):
             if letter in row:
                 letter_index = row.index(letter)
-                steps += abs(current_position[0] - i) + abs(current_position[1] - letter_index) + 1  # +1 за крок OK
+                steps += (
+                        abs(current_position[0] - i)
+                        + abs(current_position[1] - letter_index)
+                        + 1
+                )  # +1 за крок OK
                 current_position = [i, letter_index]
                 break
 
     return steps
 
 
-tv_remote("code")
+# flake8: noqa E501
+
+WORDS = ["ACT", "ADD", "ALL", "APE", "AND", "ANN", "ANY", "ANT", "ARE", "ART", "ASS", "BAD", "BAR", "BAT", "BAY", "BEE", "BIG", "BIT", "BOB", "BOY", "BUN",
+         "BUT", "CAN", "CAR", "CAT", "COT", "COW", "CUT", "DAD", "DAY", "DEW", "DID", "DIN", "DOG", "DON", "DOT", "DUD", "EAR", "EAT", "EEL", "EGG", "ERR",
+         "EYE", "FAG", "FAR", "FLY", "FOR", "FUN", "FUR", "GAY", "GET", "GOT", "GUM", "GUN", "GUY", "GUT", "GYM", "HAS", "HAT", "HER", "HEY", "HIM", "HIS",
+         "HIT", "HOW", "HUG", "HUN", "ICE", "INK", "ITS", "IVE", "JAN", "JET", "JOB", "JOT", "JOY", "KEY", "LAP", "LAY", "LIE", "LET", "LOG", "MAN", "MAP",
+         "MAY", "MEN", "MOM", "MUD", "MUM", "NAP", "NEW", "NOD", "NOT", "NOW", "OAR", "ODD", "OFF", "OLD", "ONE", "OUR", "OUT", "PAN", "PAL", "PAT", "PAW",
+         "PEN", "PET", "PIG", "PIT", "POT", "PRO", "PUT", "QUO", "RAG", "RAM", "RAN", "RAP", "RAT", "RED", "RIP", "ROD", "ROT", "RUN", "RUT", "SAT", "SAW",
+         "SAY", "SEA", "SEE", "SEX", "SHE", "SOY", "SUN", "SUX", "TAN", "TAT", "TEA", "THE", "TIN", "TIP", "TIT", "TON", "TOP", "TOO", "TWO", "URN", "USE",
+         "VAN", "VET", "VIP", "WAR", "WAS", "WAY", "WED", "WHO", "WHY", "WIN", "WON", "XXX", "YAK", "YAM", "YAP", "YOU", "YUM", "ZAP", "ZIP", "ZIT", "ZOO",
+         "ABLE", "ACED", "AGOG", "AHEM", "AHOY", "ALLY", "AMEN", "ANTI", "ANTS", "ANUS", "APES", "ARMY", "ARSE", "ARTY", "AVID", "AWED", "BABY", "BARS",
+         "BATS", "BAYS", "BEAR", "BEES", "BILL", "BITE", "BITS", "BLOW", "BLUE", "BOLD", "BONE", "BOOB", "BOOM", "BOSS", "BOYS", "BUFF", "BUNG", "BUNS",
+         "BUMS", "BURP", "BUST", "BUSY", "BUZZ", "CANS", "CANT", "CARS", "CART", "CATS", "CHAP", "CHIC", "CHUM", "CIAO", "CLAP", "COCK", "CODE", "COOL",
+         "COWS", "COZY", "CRAB", "CREW", "CURE", "CULT", "DADS", "DAFT", "DAWN", "DAYS", "DECK", "DEED", "DICK", "DING", "DOGS", "DOTS", "DOLL", "DOLT",
+         "DONG", "DOPE", "DOWN", "DRAW", "DUCK", "DUDE", "DUMB", "DUTY", "EARL", "EARN", "EARS", "EASY", "EATS", "EDGE", "EELS", "EGGS", "ENVY", "EPIC",
+         "EYES", "FACE", "FAGS", "FANG", "FARM", "FART", "FANS", "FAST", "FEAT", "FEET", "FISH", "FIVE", "FIZZ", "FLAG", "FLEW", "FLIP", "FLOW", "FOOD",
+         "FORT", "FUCK", "FUND", "GAIN", "GEEK", "GEMS", "GIFT", "GIRL", "GIST", "GIVE", "GLEE", "GLOW", "GOLD", "GOOD", "GOSH", "GRAB", "GRIN", "GRIT",
+         "GROT", "GROW", "GRUB", "GUNS", "GUSH", "GYMS", "HAIL", "HAIR", "HALO", "HANG", "HATS", "HEAD", "HEAL", "HEIR", "HELL", "HELP", "HERE", "HERO",
+         "HERS", "HIGH", "HIRE", "HITS", "HOLY", "HOPE", "HOST", "HUNK", "HUGE", "HUNG", "HUNS", "HURT", "ICON", "IDEA", "IDLE", "IDOL", "IOTA", "JAZZ",
+         "JERK", "JESS", "JETS", "JINX", "JOBS", "JOHN", "JOKE", "JUMP", "JUNE", "JULY", "JUNK", "JUST", "KATA", "KEYS", "KICK", "KIND", "KING", "KISS",
+         "KONG", "KNOB", "KNOW", "LARK", "LATE", "LEAN", "LICE", "LICK", "LIKE", "LION", "LIVE", "LOGS", "LOCK", "LONG", "LOOK", "LORD", "LOVE", "LUCK",
+         "LUSH", "MAKE", "MANY", "MART", "MATE", "MAXI", "MEEK", "MIKE", "MILD", "MINT", "MMMM", "MOMS", "MOOD", "MOON", "MOOT", "MUCH", "MUFF", "MUMS",
+         "MUTT", "NAPS", "NAZI", "NEAT", "NECK", "NEED", "NEWS", "NEXT", "NICE", "NICK", "NOON", "NOSE", "NOTE", "OARS", "OATS", "ONCE", "ONLY", "OPEN",
+         "ORGY", "OVAL", "OVER", "PANS", "PALS", "PART", "PAST", "PATS", "PAWS", "PEAR", "PERT", "PENS", "PETS", "PHEW", "PIPE", "PIPS", "PLAN", "PLUM",
+         "PLUS", "POET", "POOF", "POOP", "POSH", "POTS", "PROS", "PSST", "PUKE", "PUNK", "PURE", "PUSH", "PUSS", "QUAD", "QUAK", "QUID", "QUIT", "RANT",
+         "RAPE", "RAPS", "RAPT", "RATE", "RAMS", "RATS", "REAP", "RICK", "RING", "RIPE", "ROOT", "ROSE", "ROSY", "ROTS", "RUNT", "RUTS", "SAFE", "SAGE",
+         "SANE", "SAVE", "SAWS", "SEEK", "SEXY", "SHAG", "SHIT", "SICK", "SIGH", "SIRE", "SLAG", "SLIT", "SLUT", "SNAP", "SNOG", "SNUG", "SOFT", "SOON",
+         "SOUL", "SOUP", "SPRY", "STIR", "STUN", "SUCK", "SWAG", "SWAY", "TACT", "TANK", "TANS", "THAT", "THIS", "TIME", "TINS", "TINY", "TITS", "TOES",
+         "TONS", "TONY", "TOPS", "TOYS", "UBER", "URNS", "USED", "USER", "USES", "VAIN", "VAMP", "VARY", "VEIN", "VENT", "VERY", "VEST", "VIEW", "VIVA",
+         "VOLT", "VOTE", "WAFT", "WAGE", "WAKE", "WALK", "WALL", "WANG", "WANK", "WANT", "WARD", "WARM", "WARP", "WARS", "WART", "WASH", "WAVE", "WEAR",
+         "WEDS", "WEED", "WEEN", "WELD", "WHAT", "WHEE", "WHEW", "WHIP", "WHIZ", "WHOA", "WIFE", "WILL", "WIND", "WING", "WINK", "WINS", "WIRE", "WISH",
+         "WITH", "WORD", "WORK", "WRAP", "XMAN", "XMEN", "XRAY", "XTRA", "XXXX", "YANK", "YAKS", "YAMS", "YAPS", "YARD", "YARN", "YELP", "YERN", "YOKE",
+         "YOLK", "YULE", "ZANY", "ZAPS", "ZIPS", "ZITS", "ZERO", "ZOOM", "ZOOS"]
+
+
+def check_1800(string: str) -> set:
+    panel = {
+        "2": "ABC",
+        "3": "DEF",
+        "4": "GHI",
+        "5": "JKL",
+        "6": "MNO",
+        "7": "PQRS",
+        "8": "TUV",
+        "9": "WXYZ",
+    }
+    codes = {str(i): [] for i in range(10_000)}
+    for word in WORDS:
+        code = ""
+        for letter in word:
+            for number in panel:
+                if letter in panel[number]:
+                    code += number
+        codes[code].append(word)
+    cleared = {k: codes[k] for k in codes.keys() if len(codes[k])}
+    del codes
+    code_from_string = ""
+    for word in string.split("-")[2::]:
+        for letter in word:
+            for button in panel:
+                if letter in panel[button]:
+                    code_from_string += button
+    codes_to_process = [
+        [code_from_string[:4:], code_from_string[4::]],
+        [code_from_string[:3:], code_from_string[3::]],
+    ]
+    result = []
+
+    for code_pair in codes_to_process:
+        code1, code2 = code_pair
+        words1 = cleared.get(code1, [])
+        words2 = cleared.get(code2, [])
+        combinations = [(word1, word2) for word1 in words1 for word2 in words2]
+        for combination in combinations:
+            result.append(f"1-800-{combination[0]}-{combination[1]}")
+
+    return set(result)
+
+
+check_1800("1-800-CODE-WAR")
+# "{'1-800-INK-WANT', '1-800-HOLY-ANT', '1-800-HOLY-COT'}"
