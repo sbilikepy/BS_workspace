92a9f5d75b80aa3a76d41de6d364e0c40ca86940
Serhiienko Bohdan <serhiienko.b.i@gmail.com>
2024-01-10 20:59:45 +0200

    daily

--- Lessons/Django_WEB/daily_web.py
+++ Lessons/Django_WEB/daily_web.py
@@ -43,7 +43,8 @@ def pluck(dicts: list, name: str) -> list:
 
 
 def xo(string: str) -> bool:
-    return True if string.lower().count("x") == string.lower().count("o") else False
+    return True if string.lower().count("x") == string.lower().count(
+        "o") else False
 
 
 class PaginationHelper:
@@ -150,7 +151,8 @@ def compression(chars: list) -> str:
 
 
 def sum_of_a_beach(beach: str) -> int:
-    return sum([beach.lower().count(i) for i in ("sand", "water", "fish", "sun")])
+    return sum(
+        [beach.lower().count(i) for i in ("sand", "water", "fish", "sun")])
 
 
 def valpa():
@@ -226,7 +228,8 @@ def find_unknown_number(first: int, second: int, third: int) -> int:
                     return i
 
 
-def discover_original_price(discounted_price: float, sale_percentage: float) -> float:
+def discover_original_price(discounted_price: float,
+                            sale_percentage: float) -> float:
     return round((discounted_price / (100 - sale_percentage)) * 100, 2)
 
 
@@ -440,37 +443,98 @@ def tv_remote(word: str) -> int:
 
 # flake8: noqa E501
 
-WORDS = ["ACT", "ADD", "ALL", "APE", "AND", "ANN", "ANY", "ANT", "ARE", "ART", "ASS", "BAD", "BAR", "BAT", "BAY", "BEE", "BIG", "BIT", "BOB", "BOY", "BUN",
-         "BUT", "CAN", "CAR", "CAT", "COT", "COW", "CUT", "DAD", "DAY", "DEW", "DID", "DIN", "DOG", "DON", "DOT", "DUD", "EAR", "EAT", "EEL", "EGG", "ERR",
-         "EYE", "FAG", "FAR", "FLY", "FOR", "FUN", "FUR", "GAY", "GET", "GOT", "GUM", "GUN", "GUY", "GUT", "GYM", "HAS", "HAT", "HER", "HEY", "HIM", "HIS",
-         "HIT", "HOW", "HUG", "HUN", "ICE", "INK", "ITS", "IVE", "JAN", "JET", "JOB", "JOT", "JOY", "KEY", "LAP", "LAY", "LIE", "LET", "LOG", "MAN", "MAP",
-         "MAY", "MEN", "MOM", "MUD", "MUM", "NAP", "NEW", "NOD", "NOT", "NOW", "OAR", "ODD", "OFF", "OLD", "ONE", "OUR", "OUT", "PAN", "PAL", "PAT", "PAW",
-         "PEN", "PET", "PIG", "PIT", "POT", "PRO", "PUT", "QUO", "RAG", "RAM", "RAN", "RAP", "RAT", "RED", "RIP", "ROD", "ROT", "RUN", "RUT", "SAT", "SAW",
-         "SAY", "SEA", "SEE", "SEX", "SHE", "SOY", "SUN", "SUX", "TAN", "TAT", "TEA", "THE", "TIN", "TIP", "TIT", "TON", "TOP", "TOO", "TWO", "URN", "USE",
-         "VAN", "VET", "VIP", "WAR", "WAS", "WAY", "WED", "WHO", "WHY", "WIN", "WON", "XXX", "YAK", "YAM", "YAP", "YOU", "YUM", "ZAP", "ZIP", "ZIT", "ZOO",
-         "ABLE", "ACED", "AGOG", "AHEM", "AHOY", "ALLY", "AMEN", "ANTI", "ANTS", "ANUS", "APES", "ARMY", "ARSE", "ARTY", "AVID", "AWED", "BABY", "BARS",
-         "BATS", "BAYS", "BEAR", "BEES", "BILL", "BITE", "BITS", "BLOW", "BLUE", "BOLD", "BONE", "BOOB", "BOOM", "BOSS", "BOYS", "BUFF", "BUNG", "BUNS",
-         "BUMS", "BURP", "BUST", "BUSY", "BUZZ", "CANS", "CANT", "CARS", "CART", "CATS", "CHAP", "CHIC", "CHUM", "CIAO", "CLAP", "COCK", "CODE", "COOL",
-         "COWS", "COZY", "CRAB", "CREW", "CURE", "CULT", "DADS", "DAFT", "DAWN", "DAYS", "DECK", "DEED", "DICK", "DING", "DOGS", "DOTS", "DOLL", "DOLT",
-         "DONG", "DOPE", "DOWN", "DRAW", "DUCK", "DUDE", "DUMB", "DUTY", "EARL", "EARN", "EARS", "EASY", "EATS", "EDGE", "EELS", "EGGS", "ENVY", "EPIC",
-         "EYES", "FACE", "FAGS", "FANG", "FARM", "FART", "FANS", "FAST", "FEAT", "FEET", "FISH", "FIVE", "FIZZ", "FLAG", "FLEW", "FLIP", "FLOW", "FOOD",
-         "FORT", "FUCK", "FUND", "GAIN", "GEEK", "GEMS", "GIFT", "GIRL", "GIST", "GIVE", "GLEE", "GLOW", "GOLD", "GOOD", "GOSH", "GRAB", "GRIN", "GRIT",
-         "GROT", "GROW", "GRUB", "GUNS", "GUSH", "GYMS", "HAIL", "HAIR", "HALO", "HANG", "HATS", "HEAD", "HEAL", "HEIR", "HELL", "HELP", "HERE", "HERO",
-         "HERS", "HIGH", "HIRE", "HITS", "HOLY", "HOPE", "HOST", "HUNK", "HUGE", "HUNG", "HUNS", "HURT", "ICON", "IDEA", "IDLE", "IDOL", "IOTA", "JAZZ",
-         "JERK", "JESS", "JETS", "JINX", "JOBS", "JOHN", "JOKE", "JUMP", "JUNE", "JULY", "JUNK", "JUST", "KATA", "KEYS", "KICK", "KIND", "KING", "KISS",
-         "KONG", "KNOB", "KNOW", "LARK", "LATE", "LEAN", "LICE", "LICK", "LIKE", "LION", "LIVE", "LOGS", "LOCK", "LONG", "LOOK", "LORD", "LOVE", "LUCK",
-         "LUSH", "MAKE", "MANY", "MART", "MATE", "MAXI", "MEEK", "MIKE", "MILD", "MINT", "MMMM", "MOMS", "MOOD", "MOON", "MOOT", "MUCH", "MUFF", "MUMS",
-         "MUTT", "NAPS", "NAZI", "NEAT", "NECK", "NEED", "NEWS", "NEXT", "NICE", "NICK", "NOON", "NOSE", "NOTE", "OARS", "OATS", "ONCE", "ONLY", "OPEN",
-         "ORGY", "OVAL", "OVER", "PANS", "PALS", "PART", "PAST", "PATS", "PAWS", "PEAR", "PERT", "PENS", "PETS", "PHEW", "PIPE", "PIPS", "PLAN", "PLUM",
-         "PLUS", "POET", "POOF", "POOP", "POSH", "POTS", "PROS", "PSST", "PUKE", "PUNK", "PURE", "PUSH", "PUSS", "QUAD", "QUAK", "QUID", "QUIT", "RANT",
-         "RAPE", "RAPS", "RAPT", "RATE", "RAMS", "RATS", "REAP", "RICK", "RING", "RIPE", "ROOT", "ROSE", "ROSY", "ROTS", "RUNT", "RUTS", "SAFE", "SAGE",
-         "SANE", "SAVE", "SAWS", "SEEK", "SEXY", "SHAG", "SHIT", "SICK", "SIGH", "SIRE", "SLAG", "SLIT", "SLUT", "SNAP", "SNOG", "SNUG", "SOFT", "SOON",
-         "SOUL", "SOUP", "SPRY", "STIR", "STUN", "SUCK", "SWAG", "SWAY", "TACT", "TANK", "TANS", "THAT", "THIS", "TIME", "TINS", "TINY", "TITS", "TOES",
-         "TONS", "TONY", "TOPS", "TOYS", "UBER", "URNS", "USED", "USER", "USES", "VAIN", "VAMP", "VARY", "VEIN", "VENT", "VERY", "VEST", "VIEW", "VIVA",
-         "VOLT", "VOTE", "WAFT", "WAGE", "WAKE", "WALK", "WALL", "WANG", "WANK", "WANT", "WARD", "WARM", "WARP", "WARS", "WART", "WASH", "WAVE", "WEAR",
-         "WEDS", "WEED", "WEEN", "WELD", "WHAT", "WHEE", "WHEW", "WHIP", "WHIZ", "WHOA", "WIFE", "WILL", "WIND", "WING", "WINK", "WINS", "WIRE", "WISH",
-         "WITH", "WORD", "WORK", "WRAP", "XMAN", "XMEN", "XRAY", "XTRA", "XXXX", "YANK", "YAKS", "YAMS", "YAPS", "YARD", "YARN", "YELP", "YERN", "YOKE",
-         "YOLK", "YULE", "ZANY", "ZAPS", "ZIPS", "ZITS", "ZERO", "ZOOM", "ZOOS"]
+WORDS = ["ACT", "ADD", "ALL", "APE", "AND", "ANN", "ANY", "ANT", "ARE", "ART",
+         "ASS", "BAD", "BAR", "BAT", "BAY", "BEE", "BIG", "BIT", "BOB", "BOY",
+         "BUN",
+         "BUT", "CAN", "CAR", "CAT", "COT", "COW", "CUT", "DAD", "DAY", "DEW",
+         "DID", "DIN", "DOG", "DON", "DOT", "DUD", "EAR", "EAT", "EEL", "EGG",
+         "ERR",
+         "EYE", "FAG", "FAR", "FLY", "FOR", "FUN", "FUR", "GAY", "GET", "GOT",
+         "GUM", "GUN", "GUY", "GUT", "GYM", "HAS", "HAT", "HER", "HEY", "HIM",
+         "HIS",
+         "HIT", "HOW", "HUG", "HUN", "ICE", "INK", "ITS", "IVE", "JAN", "JET",
+         "JOB", "JOT", "JOY", "KEY", "LAP", "LAY", "LIE", "LET", "LOG", "MAN",
+         "MAP",
+         "MAY", "MEN", "MOM", "MUD", "MUM", "NAP", "NEW", "NOD", "NOT", "NOW",
+         "OAR", "ODD", "OFF", "OLD", "ONE", "OUR", "OUT", "PAN", "PAL", "PAT",
+         "PAW",
+         "PEN", "PET", "PIG", "PIT", "POT", "PRO", "PUT", "QUO", "RAG", "RAM",
+         "RAN", "RAP", "RAT", "RED", "RIP", "ROD", "ROT", "RUN", "RUT", "SAT",
+         "SAW",
+         "SAY", "SEA", "SEE", "SEX", "SHE", "SOY", "SUN", "SUX", "TAN", "TAT",
+         "TEA", "THE", "TIN", "TIP", "TIT", "TON", "TOP", "TOO", "TWO", "URN",
+         "USE",
+         "VAN", "VET", "VIP", "WAR", "WAS", "WAY", "WED", "WHO", "WHY", "WIN",
+         "WON", "XXX", "YAK", "YAM", "YAP", "YOU", "YUM", "ZAP", "ZIP", "ZIT",
+         "ZOO",
+         "ABLE", "ACED", "AGOG", "AHEM", "AHOY", "ALLY", "AMEN", "ANTI",
+         "ANTS", "ANUS", "APES", "ARMY", "ARSE", "ARTY", "AVID", "AWED",
+         "BABY", "BARS",
+         "BATS", "BAYS", "BEAR", "BEES", "BILL", "BITE", "BITS", "BLOW",
+         "BLUE", "BOLD", "BONE", "BOOB", "BOOM", "BOSS", "BOYS", "BUFF",
+         "BUNG", "BUNS",
+         "BUMS", "BURP", "BUST", "BUSY", "BUZZ", "CANS", "CANT", "CARS",
+         "CART", "CATS", "CHAP", "CHIC", "CHUM", "CIAO", "CLAP", "COCK",
+         "CODE", "COOL",
+         "COWS", "COZY", "CRAB", "CREW", "CURE", "CULT", "DADS", "DAFT",
+         "DAWN", "DAYS", "DECK", "DEED", "DICK", "DING", "DOGS", "DOTS",
+         "DOLL", "DOLT",
+         "DONG", "DOPE", "DOWN", "DRAW", "DUCK", "DUDE", "DUMB", "DUTY",
+         "EARL", "EARN", "EARS", "EASY", "EATS", "EDGE", "EELS", "EGGS",
+         "ENVY", "EPIC",
+         "EYES", "FACE", "FAGS", "FANG", "FARM", "FART", "FANS", "FAST",
+         "FEAT", "FEET", "FISH", "FIVE", "FIZZ", "FLAG", "FLEW", "FLIP",
+         "FLOW", "FOOD",
+         "FORT", "FUCK", "FUND", "GAIN", "GEEK", "GEMS", "GIFT", "GIRL",
+         "GIST", "GIVE", "GLEE", "GLOW", "GOLD", "GOOD", "GOSH", "GRAB",
+         "GRIN", "GRIT",
+         "GROT", "GROW", "GRUB", "GUNS", "GUSH", "GYMS", "HAIL", "HAIR",
+         "HALO", "HANG", "HATS", "HEAD", "HEAL", "HEIR", "HELL", "HELP",
+         "HERE", "HERO",
+         "HERS", "HIGH", "HIRE", "HITS", "HOLY", "HOPE", "HOST", "HUNK",
+         "HUGE", "HUNG", "HUNS", "HURT", "ICON", "IDEA", "IDLE", "IDOL",
+         "IOTA", "JAZZ",
+         "JERK", "JESS", "JETS", "JINX", "JOBS", "JOHN", "JOKE", "JUMP",
+         "JUNE", "JULY", "JUNK", "JUST", "KATA", "KEYS", "KICK", "KIND",
+         "KING", "KISS",
+         "KONG", "KNOB", "KNOW", "LARK", "LATE", "LEAN", "LICE", "LICK",
+         "LIKE", "LION", "LIVE", "LOGS", "LOCK", "LONG", "LOOK", "LORD",
+         "LOVE", "LUCK",
+         "LUSH", "MAKE", "MANY", "MART", "MATE", "MAXI", "MEEK", "MIKE",
+         "MILD", "MINT", "MMMM", "MOMS", "MOOD", "MOON", "MOOT", "MUCH",
+         "MUFF", "MUMS",
+         "MUTT", "NAPS", "NAZI", "NEAT", "NECK", "NEED", "NEWS", "NEXT",
+         "NICE", "NICK", "NOON", "NOSE", "NOTE", "OARS", "OATS", "ONCE",
+         "ONLY", "OPEN",
+         "ORGY", "OVAL", "OVER", "PANS", "PALS", "PART", "PAST", "PATS",
+         "PAWS", "PEAR", "PERT", "PENS", "PETS", "PHEW", "PIPE", "PIPS",
+         "PLAN", "PLUM",
+         "PLUS", "POET", "POOF", "POOP", "POSH", "POTS", "PROS", "PSST",
+         "PUKE", "PUNK", "PURE", "PUSH", "PUSS", "QUAD", "QUAK", "QUID",
+         "QUIT", "RANT",
+         "RAPE", "RAPS", "RAPT", "RATE", "RAMS", "RATS", "REAP", "RICK",
+         "RING", "RIPE", "ROOT", "ROSE", "ROSY", "ROTS", "RUNT", "RUTS",
+         "SAFE", "SAGE",
+         "SANE", "SAVE", "SAWS", "SEEK", "SEXY", "SHAG", "SHIT", "SICK",
+         "SIGH", "SIRE", "SLAG", "SLIT", "SLUT", "SNAP", "SNOG", "SNUG",
+         "SOFT", "SOON",
+         "SOUL", "SOUP", "SPRY", "STIR", "STUN", "SUCK", "SWAG", "SWAY",
+         "TACT", "TANK", "TANS", "THAT", "THIS", "TIME", "TINS", "TINY",
+         "TITS", "TOES",
+         "TONS", "TONY", "TOPS", "TOYS", "UBER", "URNS", "USED", "USER",
+         "USES", "VAIN", "VAMP", "VARY", "VEIN", "VENT", "VERY", "VEST",
+         "VIEW", "VIVA",
+         "VOLT", "VOTE", "WAFT", "WAGE", "WAKE", "WALK", "WALL", "WANG",
+         "WANK", "WANT", "WARD", "WARM", "WARP", "WARS", "WART", "WASH",
+         "WAVE", "WEAR",
+         "WEDS", "WEED", "WEEN", "WELD", "WHAT", "WHEE", "WHEW", "WHIP",
+         "WHIZ", "WHOA", "WIFE", "WILL", "WIND", "WING", "WINK", "WINS",
+         "WIRE", "WISH",
+         "WITH", "WORD", "WORK", "WRAP", "XMAN", "XMEN", "XRAY", "XTRA",
+         "XXXX", "YANK", "YAKS", "YAMS", "YAPS", "YARD", "YARN", "YELP",
+         "YERN", "YOKE",
+         "YOLK", "YULE", "ZANY", "ZAPS", "ZIPS", "ZITS", "ZERO", "ZOOM",
+         "ZOOS"]
 
 
 def check_1800(string: str) -> set:
@@ -518,6 +582,8 @@ def check_1800(string: str) -> set:
 
 
 check_1800("1-800-CODE-WAR")
+
+
 # "{'1-800-INK-WANT', '1-800-HOLY-ANT', '1-800-HOLY-COT'}"
 
 
@@ -528,7 +594,6 @@ def duplicate_arguments(*args) -> bool:
         return True
 
 
-
 def str_in_str(haystack: str, needle: str) -> int:
     if not len(needle):
         return 0
@@ -536,3 +601,9 @@ def str_in_str(haystack: str, needle: str) -> int:
         return haystack.index(needle)
     except Exception:
         return -1
+
+
+def count_ones(left: int, right: int) -> int:
+    return sum(bin(i).count("1") for i in range(left, right + 1))
+
+
