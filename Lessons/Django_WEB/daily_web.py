30d2896d618fe00de8f613d7e9137b96dffb09bb
Serhiienko Bohdan <serhiienko.b.i@gmail.com>
2024-02-23 19:50:46 +0200

    binary

--- Lessons/Django_WEB/daily_web.py
+++ Lessons/Django_WEB/daily_web.py
@@ -1501,3 +1501,38 @@ def choose_best_sum(limit: int, number_of_towns: int,
             if best_sum is None or current_sum > best_sum:
                 best_sum = current_sum
     return best_sum
+
+
+# Write a Python program to find the kth smallest element in a given binary search tree
+
+class TreeNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+def kth_smallest(root, k):
+    stack = []
+    while root or stack:
+        while root:
+            stack.append(root)
+            root = root.left
+        root = stack.pop()
+        k -= 1
+        if k == 0:
+            break
+        root = root.right
+    return root.val
+
+
+root = TreeNode(8)
+root.left = TreeNode(5)
+root.right = TreeNode(14)
+root.left.left = TreeNode(4)
+root.left.right = TreeNode(6)
+root.left.right.left = TreeNode(8)
+root.left.right.right = TreeNode(7)
+root.right.right = TreeNode(24)
+root.right.right.left = TreeNode(22)
+
