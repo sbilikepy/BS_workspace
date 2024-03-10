0670058cff3e0ef8d6b4c2577b2bf1caab2e8f64
Serhiienko Bohdan <serhiienko.b.i@gmail.com>
2024-03-10 18:29:09 +0200

    daily

--- Lessons/Django_WEB/daily_web.py
+++ Lessons/Django_WEB/daily_web.py
@@ -1732,3 +1732,50 @@ class Player:
             print("You move to", self.current_room.description)
         else:
             print("There is no door in that direction!")
+
+
+
+import random
+
+def choose_word():
+    words = ['apple', 'banana', 'orange', 'grape', 'pineapple']
+    return random.choice(words)
+
+def display_word(word, guessed_letters):
+    display = ''
+    for letter in word:
+        if letter in guessed_letters:
+            display += letter
+        else:
+            display += '_'
+    return display
+
+def main():
+    word = choose_word()
+    guessed_letters = []
+    attempts = 6
+
+    print("Welcome to the Word Guessing Game!")
+    print("Try to guess the word one letter at a time.")
+
+    while True:
+        print("\nWord:", display_word(word, guessed_letters))
+        print("Attempts left:", attempts)
+        guess = input("Enter a letter: ").lower()
+
+        if guess in guessed_letters:
+            print("You've already guessed that letter.")
+        else:
+            guessed_letters.append(guess)
+            if guess not in word:
+                attempts -= 1
+                print("Incorrect guess!")
+                if attempts == 0:
+                    print("You've run out of attempts. The word was:", word)
+                    break
+            else:
+                print("Correct guess!")
+
+        if set(word) == set(guessed_letters):
+            print("Congratulations! You've guessed the word correctly:", word)
+            break
