daff3368a62a90d946ebff48ac927c4f2f943da0
Serhiienko Bohdan <serhiienko.b.i@gmail.com>
2024-05-01 19:10:59 +0300

    daily js

--- Lessons/Django_WEB/daily_web.py
+++ Lessons/Django_WEB/daily_web.py
@@ -1665,3 +1665,12 @@ remove_duplicates([0, 0, 0, 2, 2, 2])  # 4
 #     return Array.from({ length: N }, (_, index) => index + 1);
 #   }
 # }
+
+
+# function getArraysSum(arr1, arr2) {
+#   let sum = 0;
+#   for (let i = 0; i < arr1.length; i++) {
+#     sum += arr1[i] + arr2[i];
+#   }
+#   return sum;
+# }
