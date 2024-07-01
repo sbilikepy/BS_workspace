e0117edd88dc54c892e69d4018c948beb92d636f
Serhiienko Bohdan <serhiienko.b.i@gmail.com>
2024-07-01 19:11:48 +0300

    daily algo

--- Lessons/Algorithms and Data Structures/algo daily.js
+++ Lessons/Algorithms and Data Structures/algo daily.js
@@ -26,3 +26,26 @@ function sqrt(x) {
 
   return right;
 }
+
+
+/**
+ * @param {number[]} arr
+ *
+ * @returns {number}
+ */
+function peakIndexInMountainArray(arr) {
+  let left = 0;
+  let right = arr.length - 1;
+
+  while (left < right) {
+    const mid = Math.floor((left + right) / 2);
+
+    if (arr[mid] < arr[mid + 1]) {
+      left = mid + 1;
+    } else {
+      right = mid;
+    }
+  }
+
+  return left;
+}
