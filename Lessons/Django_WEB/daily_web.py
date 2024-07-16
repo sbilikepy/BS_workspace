a80eab1496465f3bc1f3425818b82ab20e95c8b9
Serhiienko Bohdan <serhiienko.b.i@gmail.com>
2024-07-16 22:28:39 +0300

    daily LA

--- Lessons/Django_WEB/daily_web.py
+++ Lessons/Django_WEB/daily_web.py
@@ -2391,3 +2391,15 @@ def solve(m: float) -> float:
 
 print(solve(2.0))
 print(solve(8.0))
+
+
+
+import numpy as np
+v = np.arange(7)
+result = np.linalg.norm(v)
+print("Vector norm:")
+print(result)
+m = np.matrix('1, 2; 3, 4')
+result1 = np.linalg.norm(m)
+print("Matrix norm:")
+print(result1)
